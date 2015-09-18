2359f1a97662c5fa8d48a7669fa1e1d76b7b0da5
kamlys <lysek.kamil@gmail.com>
2015-09-18 21:32:32 +0200

    User build and product list

--- BusinessGame/BusinessGame/Controllers/HomeController.cs
+++ BusinessGame/BusinessGame/Controllers/HomeController.cs
@@ -4,15 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessGame.Models;
+using BusinessGame.ViewModels.BuildProduct;
 
 namespace BusinessGame.Controllers
 {
     public class HomeController : Controller
     {
+        game_BusinessDb db = new game_BusinessDb();
+
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                int uID = db.Users.First(u => u.Login == User.Identity.Name).ID;
+                UserBuildingProduct UserProductBuilding = new UserBuildingProduct();
+                UserProductBuilding.UBuilding = new List<UserBuildings>();
+            //    UserProductBuilding.UProduct = new List<UserProducts>();
+                UserProductBuilding.UBuilding.AddRange(GetBuilding(uID));
+            //    UserProductBuilding.UProduct.AddRange(GetProduct(uID));
+            //    return View(UserProductBuilding);
+                return View(UserProductBuilding);
+            }
+
             return View();
         }
+
+
+        public List<UserBuildings> GetBuilding(int uID)
+        {
+            var builds = db.UserBuildings.Where(u => u.User_ID == uID).ToList();
+           
+            return builds;
+        }
+
+        public JsonResult GetProduct()
+        {
+
+            IList<UserProducts> up = new List<UserProducts>();
+
+            int uID = db.Users.First(u => u.Login == User.Identity.Name).ID;
+            var products = db.UserProducts.Where(u => uID == u.User_ID);
+
+            foreach (var item in products)
+            {
+                up.Add(item);
+            }
+
+            return Json(up, JsonRequestBehavior.AllowGet);
+        }
+
+
+
 
         public ActionResult About()
         {
@@ -33,7 +75,6 @@ namespace BusinessGame.Controllers
             db.SaveChanges();
 
             return View();
-            //return db.UserBuildings.First(u => u.User_ID == loggedUser);
         }
 
 
---
+++ BusinessGame/BusinessGame/ViewModels/BuildProduct/UserBuildingProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessGame.Models;
+
+namespace BusinessGame.ViewModels.BuildProduct
+{
+    public class UserBuildingProduct
+    {
+        public List<UserBuildings> UBuilding { get; set; }
+
+        public List<UserProducts> UProduct { get; set; }
+    }
+}
